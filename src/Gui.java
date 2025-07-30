b7858de2ad3f877cdc53dc3f5fa62188d0cc5c05
Raphael L <oraphalmr@gmail.com>
2025-07-30 19:42:10 -0300

    add counter de bombas

--- src/Gui.java
+++ src/Gui.java
@@ -1,5 +1,5 @@
-import javax.lang.model.util.Elements;
 import javax.swing.*;
+import javax.swing.ImageIcon;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -7,10 +7,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Gui {
-
+    public static int qntFlags;
     public static boolean ganhou = false;
     public static boolean derrota = false;
-    public static String estadoPartida = "status";
+    public static String estadoPartida = ":P";
+    static ImageIcon ChikawaStatus = new ImageIcon("C:\\Users\\Public\\Downloads\\chikawaHappy.png");
+    static ImageIcon ChikawaNeutral = new ImageIcon("C:\\Users\\Public\\Downloads\\chikawaHappy.png");
+    static ImageIcon ChikawaSad = new ImageIcon("C:\\Users\\Public\\Downloads\\chikawaHappy.png");
+
 
     public static void updateCells(java.util.List<Cell> campo, int indexCellAtual, JButton botaoAcao){
         if (campo.get(indexCellAtual).revelada){
@@ -63,6 +67,13 @@ public class Gui {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel buttonPanel = new JPanel();
         JPanel containerPanel = new JPanel();
+
+        JLabel numeroBombas = new JLabel(Integer.toString(camp.qntBombas));
+        numeroBombas.setForeground(Color.RED);
+
+        JLabel estadoChikawa = new JLabel(ChikawaStatus);
+        estadoChikawa.setSize(50, 50);
+
         frame.setSize(55*campoTamanhoX, 55*campoTamanhoX);
 
         JLabel status = new JLabel(estadoPartida);
@@ -79,6 +90,7 @@ public class Gui {
         buttonPanel.setLayout(new GridLayout(campoTamanhoX,campoTamanhoY));
         for (int i=0; i < (campoTamanhoX*campoTamanhoY); i++) {
 
+            int finalI = i;
 
 
             JButton acao = new JButton("");
@@ -101,10 +113,27 @@ public class Gui {
             });
 
             //escutando por inputs
-            int finalI = i;
+
             acao.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
+
+                    status.setText(":P");
+                    if (ganhou){
+                        status.setText(":3");
+                        estadoChikawa.setIcon(ChikawaStatus);
+
+                    }else if(derrota){
+                        status.setText("D:");
+                        estadoChikawa.setIcon(ChikawaSad);
+
+                    }
+                }
+            });
+
+            acao.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
 
                     if (SwingUtilities.isLeftMouseButton(e) && !ganhou && !derrota){
                         boolean perdeu= campo.get(finalI).revelar(campo, listaBomba, campoTamanhoX, campoTamanhoY);
@@ -124,35 +153,41 @@ public class Gui {
                             acao.setBackground(Color.GREEN);
                             acao.setForeground(Color.GREEN);
                             campo.get(finalI).bandeira = true;
+                            qntFlags++;
                         } else if (campo.get(finalI).bandeira){
                             campo.get(finalI).bandeira = false;
                             acao.setBackground(Color.BLACK);
                             acao.setForeground(Color.BLACK);
+                            qntFlags--;
                         }
                     }
                     ganhou = camp.checarVitoria();
                     if (ganhou){
                         status.setText(":3");
-
+                        estadoChikawa.setIcon(ChikawaStatus);
                     }else if(derrota){
                         status.setText("D:");
-
+                        estadoChikawa.setIcon(ChikawaSad);
                     }else{
                         status.setText(":O");
+                        estadoChikawa.setIcon(ChikawaNeutral);
 
                     }
-
-
+                    numeroBombas.setText(Integer.toString(camp.qntBombas - qntFlags));
                 }
             });
         }
         buttonPanel.setPreferredSize(new Dimension(55*campoTamanhoX, 55*campoTamanhoY));
-        containerPanel.add(buttonPanel);
+
 
         status.setText((estadoPartida));
-        containerPanel.add(reset);
         containerPanel.add(status);
 
+
+        containerPanel.add(buttonPanel);
+
+        containerPanel.add(numeroBombas);
+        containerPanel.add(reset);
 
         frame.getContentPane().add(containerPanel);
         frame.pack();
